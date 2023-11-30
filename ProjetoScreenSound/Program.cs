cd6b40b26fc5cf97d58131b0c3195114b551532f
Matheus-Emanoel-Souza <152511027+Matheus-Emanoel-Souza@users.noreply.github.com>
2023-11-30 14:10:26 -0300

    Variáveis do tipo texto
    Foi criado as funções: ExibirMensagemDeBoasVindas() e ExibirOpcoesDoMenu().

--- ProjetoScreenSound/Program.cs
+++ ProjetoScreenSound/Program.cs
@@ -1,12 +1,29 @@
 ﻿// Screen Sound
-
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+List<string> listaDasBandas = new List<string> { "U2", "The Beatles", "Calypso" };
 
 void ExibirMensagemDeBoasVindas()
 {
-    Console.WriteLine("**************************");
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
     Console.WriteLine(mensagemDeBoasVindas);
-    Console.WriteLine("**************************");
+}
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("Digite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
 }
 
+
 ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
