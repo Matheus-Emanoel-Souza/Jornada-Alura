5c9c1898e11bbdc7483f8884c78caf8bafcefb43
Matheus-Emanoel-Souza <152511027+Matheus-Emanoel-Souza@users.noreply.github.com>
2023-12-04 09:23:38 -0300

    Orientação Objeto Modulo 1 Aula 4
    Apresentado a ideia de Classes.

--- ProjetoScreenSound/ScreenSound.OrientaçãoObjeto/ScreenSound.OrientaçãoObjeto/Program.cs
+++ ProjetoScreenSound/ScreenSound.OrientaçãoObjeto/ScreenSound.OrientaçãoObjeto/Program.cs
@@ -1,1 +1,15 @@
-﻿//Orientação Objeto Modulo 1 Aula 3
+﻿//Orientação Objeto Modulo 1 Aula 4
+
+Musica musica1 = new Musica(); // criei um objeto com uma classe.
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+
+Console.WriteLine($"Nomes da música {musica1.nome}");
+Console.WriteLine($"Artista: {musica1.artista}");
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+
+Console.WriteLine($"Nomes da música {musica2.nome}");
+Console.WriteLine($"Artista: {musica2.artista}");
