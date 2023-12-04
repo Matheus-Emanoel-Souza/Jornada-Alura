a4a38d698441b68935fc1418c7d56957cc23e04b
Matheus-Emanoel-Souza <152511027+Matheus-Emanoel-Souza@users.noreply.github.com>
2023-12-04 11:49:02 -0300

    Modulo 2 Aula 4 : Propries
    Ensinado e apresentado as "Propriedades" do C#

--- ProjetoScreenSound/ScreenSound.OrientaçãoObjeto/ScreenSound.OrientaçãoObjeto/Musica.cs
+++ ProjetoScreenSound/ScreenSound.OrientaçãoObjeto/ScreenSound.OrientaçãoObjeto/Musica.cs
@@ -3,23 +3,15 @@
     public string nome;
     public string artista;
     public int duracao;
-    private bool disponivel;
+    public bool Disponivel {  get; set; }
 
-    public void EscreveDisponivel(bool value)
-    {
-        disponivel = value;
-    }
-
-    public bool LeDisponivel() 
-    {
-        return disponivel;
-    }
+    
     public void ExibirFichaTecnica()
     {
         Console.WriteLine($"Nomes da música {nome}");
         Console.WriteLine($"Artista:{artista}");
         Console.WriteLine($"Duração:{duracao}");
-        if (disponivel)
+        if (Disponivel)
         {
             Console.WriteLine($"Disponível no plano");
         }
--- ProjetoScreenSound/ScreenSound.OrientaçãoObjeto/ScreenSound.OrientaçãoObjeto/Program.cs
+++ ProjetoScreenSound/ScreenSound.OrientaçãoObjeto/ScreenSound.OrientaçãoObjeto/Program.cs
@@ -1,17 +1,18 @@
-﻿//Orientação Objeto Modulo 2 Aula 3
+﻿//Orientação Objeto Modulo 2 Aula 4
 
 Musica musica1 = new Musica(); // criei um objeto com uma classe.
 musica1.nome = "Roxane";
 musica1.artista = "The Police";
 musica1.duracao = 273;
-musica1.EscreveDisponivel(false);
-
+musica1.Disponivel = true;
+Console.WriteLine(musica1.Disponivel);
 
 Musica musica2 = new Musica();
 musica2.nome = "Vertigo";
 musica2.artista = "U2";
 musica2.duracao = 273;
-musica1.EscreveDisponivel(false);
+musica2.Disponivel = false;
+Console.WriteLine(musica1.Disponivel);
 
 musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
