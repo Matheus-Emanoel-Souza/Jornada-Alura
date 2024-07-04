b6bcab5e4d649c138d71fd917e363c0fbf29b4c8
Matheus Emanoel <matheusemanoelgomessouza@hotmail.com>
2024-07-04 10:37:45 -0300

    Atualização.

---
+++ CsharpArquivos/ByteBankIO/Program.cs
@@ -0,0 +1,37 @@
+﻿using ByteBankIO;
+using System;
+using System.Text;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var enderecoDoArquivo = "contas.txt";
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor = new StreamReader(fluxoDoArquivo);
+            // var linha = leitor.ReadLine();
+            // var texto = leitor.ReadToEnd();
+            // var numero = leitor.Read();
+
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
+        }
+        Console.ReadLine();
+    }
+    static void EscreverBuffer(byte[]buffer)
+    {
+        var utf8 = new UTF8Encoding();//Conversão da codigicação de byts para texto.
+
+        var texto = utf8.GetString(buffer);
+        Console.Write(texto);
+        //foreach (byte meubyte in buffer)
+        //{
+            
+            //Console.Write(meubyte);
+            //Console.Write(" ");
+        //}
+    }
+}
