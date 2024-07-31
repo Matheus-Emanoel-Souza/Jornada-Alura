4b980789f241985eb107478d09320bdc9f6709c1
Matheus Emanoel <matheusemanoelgomessouza@hotmail.com>
2024-07-31 10:50:26 -0300

    Primeiro teste.

---
+++ ClassScore/ClassScore/Controllers/MateriaController.cs
@@ -0,0 +1,18 @@
+﻿using ClassScore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassScore.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class MateriaController : ControllerBase
+{
+    private static List<materia> materias = new List<materia>();
+    [HttpPost]
+    public void AdicionaMateria([FromBody]materia materia)
+    {
+        materias.Add(materia);
+        Console.WriteLine(materia.nome);
+        Console.WriteLine(materia.professor);
+        Console.WriteLine(materia.media);
+    }
+}
---
+++ ClassScore/ClassScore/Models/materia.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassScore.Models;
+
+public class materia
+{
+    public string nome { get; set; }
+    public double media { get; set; }
+    public string professor { get; set; }
+    //public double nota1 { get; set; }
+    //public double nota2 { get; set; }
+    //public double nota3 { get; set; }
+    //public double Semestral { get; set; }
+}
